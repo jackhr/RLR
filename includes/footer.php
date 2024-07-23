c0e66cc3cd44bd1fb4d315f3a164f2f5eae1762b
jackhr <jc2o@mac.com>
2024-07-23 11:25:36 -0400

    1) Update top 3 car selection css 2) Add contact banner back into the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,24 @@
 <footer>
 
-    <section id="contact-banner"></section>
+    <section id="contact-banner">
+        <div class="inner">
+            <h1>Have a Question? Feel free to ask...</h1>
+            <div>
+                <a href="/faq.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-help">
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+                        <path d="M12 17h.01" />
+                    </svg>
+                    <span>FAQ</span>
+                </a>
+                <a href="tel:+1 (268) 720-4581">+1 (268) 720-4581</a>
+            </div>
+        </div>
+    </section>
 
     <div class="inner">
         <div>
-            <div class="footer-nav">
-                <h6>Navigation</h6>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/book-now.php">Book Now</a></li>
-                    <li><a href="/about.php">About Us / Antigua</a></li>
-                    <li><a href="/faq.php">FAQ</a></li>
-                    <li><a href="/contact.php">Contact</a></li>
-                </ul>
-            </div>
-            <div class="footer-logo">
-                <div>
-                    <img src="/assets/images/logo.png" alt="Site Logo">
-                </div>
-            </div>
             <div class="footer-contact">
                 <h6>Contact</h6>
                 <div class="contact-link">
@@ -29,10 +29,33 @@
                     <span>Email:</span>
                     <a href="mailto:info@rlrentalsantigua.com">info@rlrentalsantigua.com</a>
                 </div>
+                <ul>
+                    <li>RL Rentals Antigua</li>
+                    <li>V.C. Bird Int'l Airport</li>
+                    <li>St. John's</li>
+                    <li>Antigua, WI</li>
+                </ul>
+            </div>
+            <div class="footer-service-hours">
+                <h6>Service hours</h6>
+                <span>Monday - Sunday</span>
+                <span>8:00 am to 6:00 pm</span>
+            </div>
+            <div class="footer-nav">
+                <div>
+                    <h6>Navigation</h6>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/book-now.php">Book Now</a></li>
+                        <li><a href="/about.php">About Us / Antigua</a></li>
+                        <li><a href="/faq.php">FAQ</a></li>
+                        <li><a href="/contact.php">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
 
-        <div id="copyright">© <?php echo date('Y'); ?> RL Rentals (Antigua) Antigua   |   US$ to EC$ Exchange at 2.7   |   For discounted rate, enter 2 or more days.</div>
+        <div id="copyright">© <?php echo date('Y'); ?> RL Rentals Antigua   |   US$ to EC$ Exchange at 2.7169   |   For discounted rate, enter 2 or more days.</div>
     </div>
 
 </footer>
