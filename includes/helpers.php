9d75554003fbfda3d2fa34888ebeb006adb521ca
jackhr <jc2o@mac.com>
2024-08-12 09:33:38 -0400

    Chnge email from no-reply@rlrentalsantigua.com to bookings@rlrentalsantigua.com

--- includes/helpers.php
+++ includes/helpers.php
@@ -95,7 +95,7 @@ function getAddOnsSubTotal($add_ons = null, $days = null, $itinerary = null, $ve
 
 function handleSendEmail($email_str = "", $body = "", $reply_to = "")
 {
-    $headers  = "From: no-reply@rlrentalsantigua.com\r\n";
+    $headers  = "From: bookings@rlrentalsantigua.com\r\n";
     if (strlen($reply_to) > 0) $headers .= "Reply-To: $reply_to\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
